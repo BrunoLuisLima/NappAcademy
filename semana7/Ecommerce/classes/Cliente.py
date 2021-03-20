86d39378faf3ac1e7cb57dec94291512244a7b6f
BrunoLuisLima <80166382+BrunoLuisLima@users.noreply.github.com>
2021-03-20 20:12:14 -0300

    Projeto OO: classes cliente e produto

---
+++ semana7/Ecommerce/classes/Cliente.py
@@ -0,0 +1,18 @@
+class Cliente:
+    def __init__(self, nome):
+        self._nome = nome
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, value):
+        self._nome = value
+
+    def __str__(self):
+        return self._nome
+
+    def __repr__(self):
+        return 'Cliente => ' + self._nome
+        
